1edbf0d87d246462430fc0854ac0ed5cdf27c3eb
Massimo Rangoni <maxappbr@gmail.com>
2014-05-18 22:16:32 -0300

    Add migrations file

--- Config/Migration/001_init_migrations.php
+++ Config/Migration/001_init_migrations.php
@@ -39,11 +39,60 @@ class InitMigrations extends CakeMigration {
                     ),
                     'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM'),
                 ),
+                'sendgrid_events' => array(
+                    'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+                    'event' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'email' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'smtp-id' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'useragent' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'ip' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'response' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'attempt' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'url' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'status' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'reason' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'type' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'newsletter_user_list_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+                    'newsletter_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+                    'newsletter_send_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+                    'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+                    'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+                    'indexes' => array(
+                        'PRIMARY' => array('column' => 'id', 'unique' => 1),
+                    ),
+                    'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM'),
+                ),
+                'sendgrid_event_categories' => array(
+                    'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+                    'sendgrid_event_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+                    'category' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+                    'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+                    'indexes' => array(
+                        'PRIMARY' => array('column' => 'id', 'unique' => 1),
+                    ),
+                    'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM'),
+                ),
+                'sendgrid_event_unique_arguments' => array(
+                    'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+                    'sendgrid_event_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+                    'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'value' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+                    'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+                    'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+                    'indexes' => array(
+                        'PRIMARY' => array('column' => 'id', 'unique' => 1),
+                    ),
+                    'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM'),
+                ),
             )
         ),
         'down' => array(
             'drop_table' => array(
-                'sendgrid_incoming_emails'
+                'sendgrid_incoming_emails',
+                'sendgrid_events',
+                'sendgrid_event_categories',
+                'sendgrid_event_unique_arguments'
             )
         )
     );
